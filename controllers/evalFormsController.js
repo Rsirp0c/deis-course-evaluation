44eb0cd932fba25cd1d735ce8ee854c5e6c2d560
Ming-Shih <minstone@gmail.com>
2023-06-13 23:59:45 +0800

    eval form controller set up

--- controllers/evalFormsController.js
+++ controllers/evalFormsController.js
@@ -1,1 +1,38 @@
+import EvalForm from "../models/evalForm";
 
+
+const getEvalFormParams = (body) => {
+    const { course, semester, professor, difficulty, rate, attendance, gradeRecieved, delivery, comment } = body;
+    return {
+        course,
+        semester,
+        professor,
+        difficulty,
+        rate,
+        attendance,
+        gradeRecieved,
+        delivery,
+        comment
+    };
+};
+
+function create (req, res, next) {
+  let newEvalForm = new EvalForm(getEvalFormParams(req.body));
+
+  newEvalForm.save()
+    .then((evalForm) => {
+      res.locals.redirect = "/evalForms";
+      res.locals.evalForm = evalForm;
+      next();
+    })
+    .catch((error) => {
+      console.log(`Error saving evalForm: ${error.message}`);
+      next(error);
+    });
+}
+
+export { create };
+
+
+// difference between await and .then() syntax
+// jsdoc implement 
