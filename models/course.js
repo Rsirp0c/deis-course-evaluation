57bea04150330ce13171d881e6df5ccd9e600d43
Ming-Shih <minstone@gmail.com>
2023-06-20 21:03:36 +0800

    update jsdoc for the data models

--- models/course.js
+++ models/course.js
@@ -1,3 +1,30 @@
+
+/**
+ * @class Course
+ * @description Generates a Course Model for the database
+ * @property {string} course - Course name: required
+ * @property {string} courseTitle - Course title: required
+ * @property {Array} professors - Professor name: required
+ * @property {Array} comments - Array of EvalForm ID linked to the {@link EvalForm} model: optional
+ * @property {number} gradeAverage - Grade average: optional
+ * @property {number} ratingAverage - Rating average: optional
+ * @property {string} courseDescription - Course description: required
+ * @property {Array} prerequisites - Prerequisites: required
+ * @property {string} _id - Course ID generated automatically
+ * @example
+ * {
+ *   "course": "COSI-21B",
+ *   "courseTitle": "Data Structures and Algorithms",
+ *   "professors": [ "Iraklis", "Antonella" ],
+ *   "comments": [ "64919ed6dc61bc03321359b9", "84919ed6dc6abc03324359b9" ],
+ *   "gradeAverage": 4.5,
+ *   "ratingAverage": 4.5,
+ *   "courseDescription": "This course introduces the fundamental concepts of data structures and algorithms. Topics include asymptotic analysis, abstract data types, lists, stacks, queues, trees, heaps, priority queues, balanced trees, graphs, sorting and searching, and hashing. The course also covers algorithm design and analysis techniques.",
+ *   "prerequisites": [ "COSI-10A", "COSI-12B" ],
+ *   "_id": "64919ed6dc61bc03321359b9",
+ *   "__v": 0
+ * }
+ */
 import { Schema, model } from "mongoose";
 
 const courseSchema = Schema({
@@ -14,7 +41,7 @@ const courseSchema = Schema({
     required: true,
   },
   comments: {
-    type: Array,
+    type: [{ type: Schema.Types.ObjectId, ref: 'EvalForm' }]  // Link to EvalForm model to populate data
   },
   gradeAverage: {
     type: Number,
--- models/evalForm.js
+++ models/evalForm.js
@@ -1,3 +1,33 @@
+/**
+ * @class EvalForm
+ * @description Generates a EvalForm Model for the database
+ * @property {Object} userId - User ID linked to the {@link User} model: optional
+ * @property {string} course - Course name: required
+ * @property {string} semester - Semester: required
+ * @property {string} professor - Professor name: required
+ * @property {string} difficulty - Difficulty: required
+ * @property {number} rate - Rating: required
+ * @property {boolean} attendance - Attendance: required
+ * @property {string} gradeRecieved - Grade recieved: optional
+ * @property {string} delivery - Delivery: required
+ * @property {string} comment - Comment: required
+ * @property {Date} date - Date generated automatically
+ * @property {string} _id - EvalForm ID generated automatically
+ * @example
+ * {
+ *   "course": "COSI-21B",
+ *   "semester": "FALL",
+ *   "professor": "Iraklis",
+ *   "difficulty": "easy", 
+ *   "rate": 5,
+ *   "attendance": true,
+ *   "delivery": "In-Person",
+ *   "comment": "good",
+ *   "date": "2023-06-20T12:42:26.673Z",
+ *   "_id": "64919ed6dc61bc03321359b9", 
+ *   "__v": 0
+ * }
+ */
 import { model, Schema } from "mongoose";
 
 const evalFormSchema = Schema({
