cf649ce16632caa5bc7061bb60be70edcad835e3
Ming-Shih <minstone@gmail.com>
2023-06-20 22:16:39 +0800

    clean up routes/index.js with new imports

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,20 +6,20 @@ const router = express.Router();
 
 import { create, read } from "../controllers/evaluationsController.js";
 
+/**
+ * Evaluations API routes
+ * GET /api/evaluations/forms
+ * POST api/v1/evaluations?course=course&semester=semester&professor=professor
+ */
 router.post("/evaluations/forms", create);
 router.get("/evaluations", read);
 
-// import { index, respondJSON, join, errorJSON } from "../controllers/coursesController";
-// const usersController = require("../controllers/usersController");
+/**
+ * Users API routes
+ */  
 
-// router.use(usersController.verifyToken);
-
-// router.get("/courses", index, respondJSON);
-// router.get(
-//   "/courses/:id/join",
-//   join,
-//   respondJSON
-// );
-// router.use(errorJSON);
+/**
+ * Courses API routes
+ */
 
 export default router;
