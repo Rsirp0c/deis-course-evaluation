11d6859e7636ffaa5795a74fbf7bdfe27b375bc3
Ming-Shih <minstone@gmail.com>
2023-06-21 22:46:54 +0800

    set up project as a monorepo project

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,7 @@ const router = express.Router();
 // import userRoutes from "./userRoutes";
 // import courseRoutes from "./courseRoutes";
 import apiRoutes from './apiRoutes.js';
-// import evaluationRoutes from "./evaluationRoutes.js";
 
-// router.use("/evaluations", evaluationRoutes);
 // router.use("/users", userRoutes);
 // router.use("/courses", courseRoutes);
 router.use('/api', apiRoutes);
