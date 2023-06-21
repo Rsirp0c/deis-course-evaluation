34ee43cdb226bac1a6e3f66ca4221f9ea9d890ea
Ming-Shih <minstone@gmail.com>
2023-06-21 20:41:09 +0800

    integrate eslint

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 
-import express from "express";
+import express from 'express';
 const router = express.Router();
 // import userRoutes from "./userRoutes";
 // import courseRoutes from "./courseRoutes";
-import apiRoutes from "./apiRoutes.js";
+import apiRoutes from './apiRoutes.js';
 // import evaluationRoutes from "./evaluationRoutes.js";
 
 // router.use("/evaluations", evaluationRoutes);
 // router.use("/users", userRoutes);
 // router.use("/courses", courseRoutes);
-router.use("/api", apiRoutes);
+router.use('/api', apiRoutes);
 
 
 
