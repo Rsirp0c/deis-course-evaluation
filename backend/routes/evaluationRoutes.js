e79b62e8015b96732f86179c2ca54e780ef8701d
Ming-Shih <minstone@gmail.com>
2023-06-22 08:20:20 +0800

    delete api route file

--- backend/routes/evaluationRoutes.js
+++ backend/routes/evaluationRoutes.js
@@ -1,7 +1,15 @@
 
 import express from 'express';
 const router = express.Router();
+import { create, read } from '../controllers/evaluationsController.js';
 
+/**
+ * Evaluations API routes
+ * GET api/evaluations/forms
+ * POST api/evaluations?course=course&semester=semester&professor=professor
+ */
+router.post('/forms', create);
+router.get('/', read);
 
 
 export default router; 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,12 @@ import express from 'express';
 const router = express.Router();
 // import userRoutes from "./userRoutes";
 // import courseRoutes from "./courseRoutes";
-import apiRoutes from './apiRoutes.js';
 
+import evaluationRoutes from './evaluationRoutes.js';
 // router.use("/users", userRoutes);
 // router.use("/courses", courseRoutes);
-router.use('/api', apiRoutes);
+
+router.use('/api/evaluations', evaluationRoutes);
 
 
 
