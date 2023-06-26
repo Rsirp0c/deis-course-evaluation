684db668caefaaef96ae3e7c14a986fc81ae291c
Ming-Shih <minstone@gmail.com>
2023-06-26 12:49:47 +0800

    monorepo setup continue

--- backend/controllers/coursesController.js
+++ backend/controllers/coursesController.js
@@ -14,16 +14,16 @@ import Course from '../models/course';
  */
 async function index(req, res, next) {
 	try {
-	  const courses = await Course.find({});
-	  res.locals.courses = courses;
-	  res.status(200).json(courses);
-	  next();
+		const courses = await Course.find({});
+		res.locals.courses = courses;
+		res.status(200).json(courses);
+		next();
 	} catch (err) {
-	  res.status(500).json({ error: err.message });
+		res.status(500).json({ error: err.message });
 	}
-  }
+}
 
-  /**
+/**
  * @todo optimization for error handling; add more filter for the sorting functionality on the main page? or create another function
  * @summary GET api/courses/courseId
  * @description Get a specific course by courseid    
@@ -32,22 +32,22 @@ async function index(req, res, next) {
  * @param {Object} req - request object
  * @param {Object} res - response object
  */
-  async function display(req, res, next) {
+async function display(req, res, next) {
 	try {
-	  const course = await Course.findById(req.param._id);
-  
-	  if (!course) {
-		res.status(404).json({ error: "Course not found" });
-		return;
-	  }
-  
-	  res.locals.course = course;
-	  res.status(200).json(course);
-	  next();
+		const course = await Course.findById(req.param._id);
+
+		if (!course) {
+			res.status(404).json({ error: 'Course not found' });
+			return;
+		}
+
+		res.locals.course = course;
+		res.status(200).json(course);
+		next();
 	} catch (err) {
-	  console.error(err);
-	  res.status(500).json({ error: err.message });
+		console.error(err);
+		res.status(500).json({ error: err.message });
 	}
-  }  
-  
-export { index, display};
+}
+
+export { index, display };
