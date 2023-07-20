190a72324a26d264603df8a2853c250beff0d81a
Ming-Shih <minstone@gmail.com>
2023-07-20 23:50:39 +0800

    get Google OAuth url function

---
+++ frontend/src/utils/getGoogleUrl.js
@@ -0,0 +1,25 @@
+export default function getGoogleUrl() {
+	const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+	const process = import.meta.env
+
+	const options = {
+		redirect_uri: process.VITE_GOOGLE_OAUTH_REDIRECT_URI,
+		client_id: process.VITE_GOOGLE_CLIENT_ID,
+		access_type: 'offline',
+		response_type: 'code',
+		prompt: 'consent',
+		scope: [
+			'https://www.googleapis.com/auth/userinfo.profile',
+			'https://www.googleapis.com/auth/userinfo.email',
+		].join(' '),
+
+
+	}
+	const qs = new URLSearchParams(options).toString();
+
+	const url = `${rootUrl}?${qs}`;
+
+	return url;
+}
+
