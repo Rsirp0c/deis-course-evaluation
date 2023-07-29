ce99297ca4c5e8ec5189463d571cf2f51587c623
Ming-Shih <minstone@gmail.com>
2023-07-29 23:17:11 +0800

    added user model

--- backend/models/evalForm.js
+++ backend/models/evalForm.js
@@ -30,50 +30,50 @@
  */
 import { model, Schema } from 'mongoose';
 
-const evalFormSchema = Schema({
-	userId: {
-		type: Schema.Types.ObjectId,
-		ref: 'User'
+const evalFormSchema = Schema(
+	{
+		userId: {
+			type: Schema.Types.ObjectId,
+			ref: 'User'
+		},
+		course: {
+			type: String,
+			required: true
+		},
+		semester: {
+			type: String,
+			required: true
+		},
+		professor: {
+			type: String,
+			required: true
+		},
+		difficulty: {
+			type: String,
+			required: true
+		},
+		rate: {
+			type: Number,
+			required: true
+		},
+		attendance: {
+			type: Boolean,
+			required: true
+		},
+		gradeRecieved: {
+			type: String
+		},
+		delivery: {
+			type: String,
+			required: true
+		},
+		comment: {
+			type: String,
+			required: true
+		}
 	},
-	course: {
-		type: String,
-		required: true
-	},
-	semester: {
-		type: String,
-		required: true
-	},
-	professor: {
-		type: String,
-		required: true
-	},
-	difficulty: {
-		type: String,
-		required: true
-	},
-	rate: {
-		type: Number,
-		required: true
-	},
-	attendance: {
-		type: Boolean,
-		required: true
-	},
-	gradeRecieved: {
-		type: String
-	},
-	delivery: {
-		type: String,
-		required: true
-	},
-	comment: {
-		type: String,
-		required: true
-	},
-	date: {
-		type: Date,
-		default: Date.now()
-	}
-});
+	{
+		timestamps: true,
+	});
 
 export default model('EvalForm', evalFormSchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-const Subscriber = require('../models/subscriber');
-const passportLocalMongoose = require('passport-local-mongoose');
-const randToken = require('rand-token');
 
-const userSchema = mongoose.Schema(
+import { model, Schema } from 'mongoose';
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+
+const userSchema = Schema(
 	{
 		name: {
 			first: {
@@ -17,97 +17,62 @@ const userSchema = mongoose.Schema(
 		},
 		email: {
 			type: String,
-			required: true,
-			lowercase: true,
+			required: [true, 'Email is required'],
 			unique: true,
+			match: [/\S+@\S+\.\S+/, 'is invalid']
 		},
-		zipCode: {
-			type: Number,
-			min: [10000, 'zip code is too short!'],
-			max: 99999,
-		},
-		// password: {
-		//   type: String,
-		//   required: true,
-		// },
-		courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
-		subscribedAccount: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Subscriber',
-		},
-		apiToken: {
-			type: String,
-		},
-		provider: {
-			type: String,
-		}
+		evals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'EvalForm' }],
+		likedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
+		hash: String,
+		salt: String,
 	},
 	{
 		timestamps: true,
 	}
 );
 
-userSchema.virtual('fullName').get(function () {
-	return `${this.name.first} ${this.name.last}`;
-});
+UserSchema.methods.setPassword = function (password) {
+	this.salt = crypto.randomBytes(16).toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
 
-userSchema.pre('save', function (next) {
-	let user = this;
-	if (user.subscribedAccount === undefined) {
-		Subscriber.findOne({ email: user.email })
-			.then((subscriber) => {
-				user.subscribedAccount = subscriber;
-				next();
-			})
-			.catch((error) => {
-				console.log(`Error in connecting subscriber: ${error.message}`);
-			});
-	} else {
-		next();
-	}
-});
+UserSchema.methods.validatePassword = function (password) {
+	var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+	return this.hash === hash;
+};
 
-userSchema.pre('save', function (next) {
-	let user = this;
-	if (!user.apiToken) user.apiToken = randToken.generate(16);
-	next();
-});
+UserSchema.methods.generateJWT = function () {
+	const today = new Date();
+	const secret = process.env.JWT_SECRET; // Create a secret for the JWT in your env file
+	const options = {
+		expiresIn: '14d',
+	};
+	return jwt.sign({
+		id: this._id,
+		username: this.name,
+	}, secret, options);
+};
 
-// userSchema.pre("save", function (next) {
-//   let user = this;
-//   bcrypt
-//     .hash(user.password, 10)
-//     .then((hash) => {
-//       user.password = hash;
-//       next();
-//     })
-//     .catch((error) => {
-//       console.log(`Error in hashing password: ${error.message}`);
-//       next(error);
-//     });
-// });
+// Return JSON representation of user to pass to frontend
+UserSchema.methods.toAuthJSON = function () {
+	return {
+		username: this.username,
+		email: this.email,
+		token: this.generateJWT(),
+	};
+};
 
-// userSchema.methods.passwordComparison = function (inputPassword) {
-//   let user = this;
-//   return bcrypt.compare(inputPassword, user.password);
-// };
-
-// user.model.js
-
-
-const User = mongoose.model('User', userSchema);
 
 // Add a custom static method to the schema to save google account data
 userSchema.statics.createAndUpdateUser = async function (data) {
 	const filter = { email: data.email };
 	if (User.exist(filter)) {
-		await User.findOneAndUpdate(filter,{ name: { first: data.given_name, last: data.family_name }});
+		await User.findOneAndUpdate(filter, { name: { first: data.given_name, last: data.family_name } });
 	} else {
 		await User.create(date);
 	}
-  };
-module.exports = User;
+};
 
-userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 
-module.exports = mongoose.model('User', userSchema);
+
+export default model('User', userSchema);
