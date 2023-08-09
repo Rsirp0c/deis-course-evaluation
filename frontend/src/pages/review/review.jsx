6783f92129db28458bc15a1797aed77c5cff7af4
Ming-Shih <minstone@gmail.com>
2023-08-09 22:38:51 +0800

    complete form page

--- frontend/src/pages/review/review.jsx
+++ frontend/src/pages/review/review.jsx
@@ -1,115 +1,84 @@
 /* eslint-disable react/prop-types */
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
+import { format } from '../../utils/formatSentence.js';
 import styles from './review.module.css';
-import DropdownSelection from './DropdownSelection.jsx';
+import DropdownSelection from './components/DropdownSelection.jsx';
+import RatingButtons from './components/RatingButtons.jsx';
+import AttendanceButtons from './components/AttendanceButtons.jsx';
+import DeliveryButtons from './components/DeliveryButtons.jsx';
+import TextBox from './components/TextBox.jsx';
+import LetterGradeDropdown from './components/LetterGradeDropdown.jsx';
 
 /**
- * Formats the course name passed through URL to be title case
- * @param {} str
- * @returns
+ * TO DO: write api request to submit form data to backend
+ * @returns {JSX.Element} Review page
  */
-function format(str) {
-	const smallWordsSet = new Set(['a', 'an', 'and', 'as', 'at', 'but', 'by', 'for', 'if', 'in', 'nor', 'of', 'on', 'or', 'so', 'the', 'to', 'up', 'yet']);
-
-	const words = str.split(' ');
-
-	let newStr = '';
-
-	for (let i = 0; i < words.length; i++) {
-		if (i === 0 || i === 1) {
-			newStr += `${words[i].toUpperCase()} `;
-		} else {
-			const word = words[i].toLowerCase();
-			if (smallWordsSet.has(word) && i !== 2) {
-				newStr += `${word.toLowerCase()} `;
-			} else {
-				newStr += `${word.charAt(0).toUpperCase() + word.slice(1)} `;
-			}
-		}
-	}
-	return newStr;
-}
-
 export default function Review() {
 	const { course } = useParams();
 	const [difficulty, setDifficulty] = useState(3);
 	const [quality, setQuality] = useState(3);
 	const [attendance, setAttendance] = useState(1);
 	const [delivery, setDelivery] = useState('In Person');
-	const numRatingButtons = [1, 2, 3, 4, 5];
+	const [grade, setGrade] = useState('');
+	const [professor, setProfessor] = useState('');
+	const [semester, setSemester] = useState('');
+	const [comment, setComment] = useState('');
 
-	function RatingButtons({ state, setState }) {
-		function RatingButton({ rate, state, setState }) {
-			function handleOnClick(e) {
-				e.preventDefault();
-				if (rate !== state) setState(rate);
-			}
-			let buttonStyle = rate === state ? styles.ratingButtonSelected : styles.ratingButton;
-			if (rate === 1) buttonStyle = `${buttonStyle} ${styles.firstRatingButton}`;
-			else if (rate === 5) {
-				buttonStyle = `${buttonStyle} ${styles.lastRatingButton}`;
-			}
-			return <button className={buttonStyle} onClick={handleOnClick} />;
-		}
-		return (
-			<div className={styles.ratingButtonsWrapper}>
-				<p className={styles.levelText}>Awful</p>
-				{numRatingButtons.map((rate) => (
-					<RatingButton key={rate} rate={rate} state={state} setState={setState} />
-				))}
-				<p className={styles.levelText}>GOAT</p>
-			</div>
-		);
-	}
-	/**
-	   * Not 'DRY' enough, cleaan up this later
-	   * @param {*} param0
-	   * @returns
-	   */
-	function AttendanceButtons({ state, setState }) {
-		function AttendanceButton({ value, state, setState }) {
-			function handleOnClick(e) {
-				e.preventDefault();
-				if (value !== state) setState(value);
-			}
-			let buttonStyle = value === state ? styles.attendanceButtonSelected : styles.attendanceButton;
-			if (value === 1) {
-				return <button className={buttonStyle = `${buttonStyle} ${styles.firstAttendanceButton}`} onClick={handleOnClick}>Yes</button>;
-			} if (value === 2) {
-				return <button className={`${buttonStyle} ${styles.lastAttendanceButton}`} onClick={handleOnClick}>No</button>;
-			}
-		}
-		return (
-			<div className={styles.attendanceButtonsWrapper}>
-				<AttendanceButton value={1} state={state} setState={setState} />
-				<AttendanceButton value={2} state={state} setState={setState} />
-			</div>
-		);
+	// TODO: get semesters and professor values from backend
+	const semesters = [
+		{ label: 'FALL 2021', value: 'FALL 2021' },
+		{ label: 'SPRING 2021', value: 'SPRING 2021' },
+		{ label: 'FALL 2020', value: 'FALL 2020' },
+		{ label: 'SPRING 2020', value: 'SPRING 2020' },
+		{ label: 'FALL 2019', value: 'FALL 2019' },
+		{ label: 'SPRING 2019', value: 'SPRING 2019' },
+		{ label: 'FALL 2018', value: 'FALL 2018' },
+		{ label: 'SPRING 2018', value: 'SPRING 2018' },
+		{ label: 'FALL 2017', value: 'FALL 2017' },
+	];
+	const professors = [
+		{ label: 'Professor 1', value: 'Professor 1' },
+		{ label: 'Professor 2', value: 'Professor 2' },
+		{ label: 'Professor 3', value: 'Professor 3' },
+	]
+
+	const letterGrades = [
+		{ label: 'A+', value: 'A+' },
+		{ label: 'A', value: 'A' },
+		{ label: 'A-', value: 'A-' },
+		{ label: 'B+', value: 'B+' },
+		{ label: 'B', value: 'B' },
+		{ label: 'B-', value: 'B-' },
+		{ label: 'C+', value: 'C+' },
+		{ label: 'C', value: 'C' },
+		{ label: 'C-', value: 'C-' },
+		{ label: 'D+', value: 'D+' },
+		{ label: 'D', value: 'D' },
+		{ label: 'D-', value: 'D-' },
+		{ label: 'F', value: 'F' },
+		{ label: 'Prefer not to say', value: ''}
+	];
+
+	function handleSubmit(event) {
+		event.preventDefault();
 	}
 
-	function DeliveryButtons({ state, setState }) {
-		function DeliveryButton({ value, state, setState }) {
-			function handleOnClick(e) {
-				e.preventDefault();
-				if (value !== state) setState(value);
-			}
-			const buttonStyle = value === state ? styles.deliveryButtonSelected : styles.deliveryButton;
-			if (value === 'In Person') {
-				return <button className={`${buttonStyle} ${styles.firstDeliveryButton}`} onClick={handleOnClick}>{value}</button>;
-			} if (value === 'Hybrid') {
-				return <button className={`${buttonStyle} ${styles.lastDeliveryButton}`} onClick={handleOnClick}>{value}</button>;
-			}
-			return <button className={`${buttonStyle}`} onClick={handleOnClick}>{value}</button>;
-		}
-		return (
-			<div className={styles.deliveryButtonsWrapper}>
-				<DeliveryButton value="In Person" state={state} setState={setState} />
-				<DeliveryButton value="Online" state={state} setState={setState} />
-				<DeliveryButton value="Hybrid" state={state} setState={setState} />
-			</div>
-		);
+	function handleProfessorChange(event, value) {
+		setProfessor(value)
+		console.log({professor})
 	}
+	function handleSemesterChange(event, value) {
+		setSemester(value)
+	}
+	function handleCommentChange(event, value) {
+		setComment(value)
+	}
+	function handleGradeChange(event, value) {
+		setGrade(value)
+	}
+
+
 	return (
 		<div className={styles.review}>
 			<div className={styles.titleContainer}>
@@ -121,9 +90,8 @@ export default function Review() {
 			<div className={styles.reviewContainer}>
 				<form className={styles.form}>
 					<div className={styles.dropdownWrapper}>
-						<DropdownSelection/>
-						<button type='button' className={styles.tempButtonStyle}>temp* Select Semester</button>
-						<button type='button' className={styles.tempButtonStyle}>temp* Select Professor</button>
+						<DropdownSelection options={semesters} label="Select Semester" handleChange={handleSemesterChange} />
+						<DropdownSelection options={professors} label="Select Professor" handleChange={handleProfessorChange}/>
 					</div>
 					<div className={styles.ratingWrapper}>
 						<h2 className={styles.ratingDesc}>Rate the difficulty of this course</h2>
@@ -137,22 +105,26 @@ export default function Review() {
 						<h2 className={styles.ratingDesc}>Attendance is requried</h2>
 						<AttendanceButtons state={attendance} setState={setAttendance} />
 					</div>
-					<div className={styles.ratingWrapper}>
-						<h2 className={styles.ratingDesc}>Your grade (optional)</h2>
-						<button className={styles.gradeDropdown} onClick={(e) => e.preventDefault()} />
-					</div>
+				
 					<div className={styles.ratingWrapper}>
 						<h2 className={styles.ratingDesc}>Delivery mode</h2>
 						<DeliveryButtons state={delivery} setState={setDelivery} />
 					</div>
-
+					<div className={styles.ratingWrapper}>
+						<h2 className={styles.ratingDesc}>Your grade (optional)</h2>
+						<LetterGradeDropdown options={letterGrades} label="grade" handleGradeChange={handleGradeChange} />
+					</div>
 					<div className={styles.dividerContainer}><div className={styles.divider} /></div>
 
 					<div className={styles.commentWrapper}>
 						<h2 className={styles.ratingDesc}>Your Comment:</h2>
-						<textarea placeholder="Write a comment..." />
+						<div className={styles.textBoxContainer}>
+							<TextBox setComment={setComment} handleCommentChange={handleCommentChange} />
+						</div>
 					</div>
-					<div className={styles.submit} />
+					<div className={styles.submit}>
+						<button className={styles.submitButton} type="submit" onClick={handleSubmit} >Submit</button>
+					</div>
 				</form>
 			</div>
 		</div>
---
+++ frontend/src/utils/formatSentence.js
@@ -0,0 +1,46 @@
+/**
+ * Formats the course name passed through URL to be title case
+ * @param {} str
+ * @returns
+ */
+export function format(str) {
+  const smallWordsSet = new Set([
+    'a',
+    'an',
+    'and',
+    'as',
+    'at',
+    'but',
+    'by',
+    'for',
+    'if',
+    'in',
+    'nor',
+    'of',
+    'on',
+    'or',
+    'so',
+    'the',
+    'to',
+    'up',
+    'yet',
+  ]);
+
+  const words = str.split(' ');
+
+  let newStr = '';
+
+  for (let i = 0; i < words.length; i++) {
+    if (i === 0 || i === 1) {
+      newStr += `${words[i].toUpperCase()} `;
+    } else {
+      const word = words[i].toLowerCase();
+      if (smallWordsSet.has(word) && i !== 2) {
+        newStr += `${word.toLowerCase()} `;
+      } else {
+        newStr += `${word.charAt(0).toUpperCase() + word.slice(1)} `;
+      }
+    }
+  }
+  return newStr;
+}
