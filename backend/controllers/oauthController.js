592dce2d76ecda751753f94976e4893f563aecc5
Ming-Shih <minstone@gmail.com>
2023-08-30 16:29:27 -0400

    revert to fetch

--- backend/controllers/oauthController.js
+++ backend/controllers/oauthController.js
@@ -23,7 +23,7 @@ async function getGoogleOAuthTokens(code) {
 			},
 			body: queryParams
 		}
-		const response = await axios(url, options);
+		const response = await fetch(url, options);
 
 		const data = await response.json();
 
