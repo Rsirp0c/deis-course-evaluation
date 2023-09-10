bec650a62e4fe0cf8c85ad02b65bac9e14632907
Ming-Shih <minstone@gmail.com>
2023-09-09 22:27:09 -0400

    code clean up

--- frontend/src/pages/review/components/RatingButtons.jsx
+++ frontend/src/pages/review/components/RatingButtons.jsx
@@ -1,22 +1,25 @@
+/* eslint-disable react/prop-types */
 import styles from './ButtonsStyles.module.css';
+
+
+function RatingButton({ rate, state, setState }) {
+	function handleOnClick(e) {
+		e.preventDefault();
+		if (rate !== state) setState(rate);
+	}
+	let buttonStyle = rate === state ? styles.ratingButtonSelected : styles.ratingButton;
+	if (rate === 1) buttonStyle = `${buttonStyle} ${styles.firstRatingButton}`;
+	else if (rate === 5) {
+		buttonStyle = `${buttonStyle} ${styles.lastRatingButton}`;
+	}
+	return <button className={buttonStyle} onClick={handleOnClick} />;
+}
+
 
 export default function RatingButtons({ state, setState }) {
 
 	const numRatingButtons = [1, 2, 3, 4, 5];
 
-	
-	function RatingButton({ rate, state, setState }) {
-		function handleOnClick(e) {
-			e.preventDefault();
-			if (rate !== state) setState(rate);
-		}
-		let buttonStyle = rate === state ? styles.ratingButtonSelected : styles.ratingButton;
-		if (rate === 1) buttonStyle = `${buttonStyle} ${styles.firstRatingButton}`;
-		else if (rate === 5) {
-			buttonStyle = `${buttonStyle} ${styles.lastRatingButton}`;
-		}
-		return <button className={buttonStyle} onClick={handleOnClick} />;
-	}
 	return (
 		<div className={styles.ratingButtonsWrapper}>
 			<p className={styles.levelText}>Awful</p>
